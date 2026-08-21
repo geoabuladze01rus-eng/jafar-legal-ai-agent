ea8910064fd4b9c5056e8b3c6168f9274763d189
geoabuladze01rus-eng <geo.abuladze01rus@gmail.com>
2026-08-21 22:14:34 +0300

    test: verify publication runtime imports

---
+++ tests/test_imports.py
@@ -0,0 +1,12 @@
+def test_publication_modules_import():
+    import jafar.publication_cycle
+    import jafar.publication_dispatch
+    import jafar.publication_recovery
+    import jafar.publication_runtime
+    import jafar.publication_worker
+    import jafar.publication_worker_store
+    import jafar.run_publication_worker
+    import jafar.supabase_publication_repository
+    import jafar.telegram_api
+
+    assert True

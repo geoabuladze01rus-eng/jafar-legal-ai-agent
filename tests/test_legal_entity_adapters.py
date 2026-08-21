57c5e9ab0cd6ff08d306527d91e8039746e66da2
geoabuladze01rus-eng <geo.abuladze01rus@gmail.com>
2026-08-21 22:46:43 +0300

    test: add legal entity public source registry

---
+++ tests/test_legal_entity_adapters.py
@@ -0,0 +1,21 @@
+from jafar.legal_entity_adapters import LegalEntitySourceRegistry, PublicSourceAdapter
+from jafar.legal_entity_intelligence import EntityQuery
+
+
+def test_registry_runs_all_public_adapters_and_preserves_failures():
+    registry = LegalEntitySourceRegistry(
+        [
+            PublicSourceAdapter("kad", "https://kad.arbitr.ru/"),
+            PublicSourceAdapter("fssp"),
+        ]
+    )
+
+    results = registry.search_all(EntityQuery("7701234567", "inn"))
+
+    assert [item.source_key for item in results] == ["kad", "fssp"]
+    assert all(item.status == "no_data" for item in results)
+
+
+def test_registry_contains_only_registered_sources():
+    registry = LegalEntitySourceRegistry([PublicSourceAdapter("egrul")])
+    assert registry.keys() == ["egrul"]

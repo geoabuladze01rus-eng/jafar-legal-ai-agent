a239e492c62702019d4bfa9fc17e0cd898782423
geoabuladze01rus-eng <geo.abuladze01rus@gmail.com>
2026-08-26 14:48:33 +0300

    Start local backend with Jafar app

--- apple/JafarApp/JafarApp.swift
+++ apple/JafarApp/JafarApp.swift
@@ -2,9 +2,15 @@ import SwiftUI
 
 @main
 struct JafarApp: App {
+    @StateObject private var localBackend = LocalBackendManager()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(localBackend)
+                .task {
+                    localBackend.startIfConfigured()
+                }
         }
     }
 }

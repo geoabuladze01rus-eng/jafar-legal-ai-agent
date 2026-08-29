070dd3e88e3a695770eecb1a25654e66b830df1e
geoabuladze01rus-eng <geo.abuladze01rus@gmail.com>
2026-08-29 06:18:13 +0300

    Configure pseudonymous Telegram poll identities

--- src/jafar/config.py
+++ src/jafar/config.py
@@ -31,6 +31,7 @@ class Settings(BaseSettings):
     telegram_polling_enabled: bool = False
     telegram_scheduler_enabled: bool = False
     telegram_scheduler_db_path: str = "var/telegram_scheduler.sqlite3"
+    telegram_poll_identity_secret: str | None = None
     telegram_production_send: bool = False
     telegram_dry_run: bool = True
     jafar_mcp_auth_token: str | None = None
